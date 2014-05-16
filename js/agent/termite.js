021d52c3141af97c2b51939847a0900447a0c11e
epayet <jakspinning@gmail.com>
2014-05-16 15:11:33 +0200

    Archi finalization

--- js/agent/termite.js
+++ js/agent/termite.js
@@ -2,107 +2,82 @@ Termite.prototype = new Agent();
 Termite.prototype.constructor = Termite;
 
 function Termite() {
-	Agent.call(this);
-	this.typeId = "termite";
-	this.boundingRadius = 3;
-	this.perceptionRadius = 100;
+    Agent.call(this);
+    this.typeId = "termite";
+    this.boundingRadius = 3;
+    this.perceptionRadius = 100;
+    this.hasWood = false;
+    this.collidedAgent = null;
 
-	this.hasWood = false;
+    this.collideTypes = ["wood_heap"];
+    this.contactTypes = ["wood_heap", "termite"];
 
-	this.collideTypes = ["wood_heap", "wall"];
-	this.contactTypes = ["wood_heap"];
-
-	this.heapInfos = [];
-	this.directionDelay = 0;
-	this.speed = 500;
-	this.updateRandomDirection();
+    this.initExpertSystem();
 }
 
-Termite.prototype.updateRandomDirection = function(dt) {
-	this.direction = new Vect(Math.random() * 2 - 1, Math.random() * 2 - 1);
-	this.direction.normalize(1);
-};
-Termite.prototype.setTarget = function(x, y) {
-	this.direction = new Vect(x - this.x, y - this.y);
-	this.direction.normalize(1);
+Termite.prototype.initExpertSystem = function() {
+    this.expertSystem = new ExpertSystem();
+    //this.expertSystem.addRule("go_to_heap", ["heap_percepted"];
 };
 
 Termite.prototype.update = function(dt) {
-	this.directionDelay -= dt;
-	if(this.directionDelay <= 0) {
-		var targetHeap = null;
-		var searchTargetHeap = (Math.random() < 0.9)
-		if(searchTargetHeap) {
-			for(identifier in this.heapInfos) {
-				var heapInfo = this.heapInfos[identifier];
-				if(heapInfo.count > 2) {
-					if(this.hasWood) {
-						if(targetHeap == null || heapInfo.count > targetHeap.count) {
-							targetHeap  = heapInfo;
-						}
-					} else if(!this.hasWood) {
-						if(targetHeap == null || heapInfo.count < targetHeap.count) {
-							targetHeap  = heapInfo;
-						}			
-					}
-				}
-			}
-		}
-		if(targetHeap) {
-			this.setTarget(targetHeap.x, targetHeap.y);
-		} else {
-			this.updateRandomDirection();
-		}
-		this.speed = 100 + Math.random() * 200;
-		this.directionDelay = 100 + Math.random() * 900;
-	}
-
-	var x = this.x + this.direction.x * this.speed * dt / 1000;
-	var y = this.y + this.direction.y * this.speed * dt / 1000;
-	this.moveTo(x,y);
+    this.perceive();
+    var conclusions = this.analyze();
+    this.act(conclusions);
+    this.resetCollidedAgent();
 };
 
-Termite.prototype.draw = function(context) {
-	context.fillStyle = this.hasWood ? "#f00" : "#000";
-	context.strokeStyle = "#000";
-	context.beginPath();
-	context.arc(this.x,this.y,this.boundingRadius,0,2*Math.PI);
-	context.fill();
-	context.stroke();
+Termite.prototype.perceive = function() {
+    this.expertSystem.resetFactValues();
+
+    this.expertSystem.setFactValid("collided_heap", this.isCollidedWith("wood_heap"));
 };
+
+Termite.prototype.analyze = function() {
+    return this.expertSystem.inferForward();
+};
+
+Termite.prototype.act = function(conclusions) {
+    for(var i = 0; i<conclusions.length; i++) {
+        if(conclusions[i] == "") {
+
+        }
+    }
+};
+
+Termite.prototype.isCollidedWith = function(agentType) {
+    return this.collidedAgent == agentType;
+};
+
 
 Termite.prototype.processCollision = function(collidedAgent) {
-	if(collidedAgent == null || collidedAgent.typeId == "wall") {
-		this.directionDelay = 0;
-
-	} else if(collidedAgent.typeId == "wood_heap") {
-		if(this.hasWood) {
-			collidedAgent.addWood();
-			this.hasWood = false;
-		} else {
-			collidedAgent.takeWood();
-			this.hasWood = true;
-		}
-		//this.changeDirection();		
-	}
+    if(collidedAgent && collidedAgent.typeId == "wood_heap") {
+        this.collidedAgent = "wood_heap";
+        if(!this.hasWood) {
+            collidedAgent.takeWood();
+            this.hasWood = true;
+        } else {
+            collidedAgent.addWood();
+            this.hasWood = false;
+        }
+    }
 };
 
 Termite.prototype.processPerception = function(perceivedAgent) {
-	if(perceivedAgent.typeId == "wood_heap") {
-		this.heapInfos[perceivedAgent.identifier] = {
-			"x":perceivedAgent.x,
-			"y":perceivedAgent.y,
-			"count": perceivedAgent.woodCount,
-			"date" : new Date()
-		};
-	} else if(perceivedAgent.typeId == "termite") {
-		for(identifier in perceivedAgent.heapInfos) {
-			var heapInfo = perceivedAgent.heapInfos[identifier];
-			if(this.heapInfos[identifier] == null) {
-				this.heapInfos[identifier] = heapInfo;
-			} else if(this.heapInfos[identifier].date < heapInfo.date)
-				this.heapInfos[identifier] = heapInfo;				
-			
-		}
-	}
+};
+
+Termite.prototype.resetCollidedAgent = function(){
+    this.collidedAgent = null;
+};
+
+Termite.prototype.draw = function(context) {
+    if(this.hasWood)
+        context.fillStyle="rgba(255, 0, 0, 1)";
+    else
+        context.fillStyle="rgba(0, 0, 0, 1)";
+    context.strokeStyle="#000";
+    context.beginPath();
+    context.arc(this.x, this.y, this.boundingRadius, 0, 2*Math.PI);
+    context.fill();
+    context.stroke();
 };
