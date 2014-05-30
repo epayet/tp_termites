4b7a30ea305fc8d217c487eaf5c1f9f69130e504
epayet <jakspinning@gmail.com>
2014-05-30 23:14:50 +0200

    Système expert + communication simple : ok sans wall

--- js/agent/termite.js
+++ js/agent/termite.js
@@ -7,9 +7,13 @@ function Termite() {
     this.boundingRadius = 3;
     this.perceptionRadius = 100;
     this.hasWood = false;
+    this.speed = 200;
     this.collidedAgent = null;
+    this.perceivedAgents = [];
+    this.delay = 0;
+    this.woodInfo = new WoodInfo();
 
-    this.collideTypes = ["wood_heap"];
+    this.collideTypes = ["wood_heap", "wall"];
     this.contactTypes = ["wood_heap", "termite"];
 
     this.initExpertSystem();
@@ -17,44 +21,89 @@ function Termite() {
 
 Termite.prototype.initExpertSystem = function() {
     this.expertSystem = new ExpertSystem();
-    //this.expertSystem.addRule("go_to_heap", ["heap_percepted"];
+    this.expertSystem.addRule("go_to_least_wood", ["has_no_wood", "know_least_wood_position"]);
+    this.expertSystem.addRule("go_to_most_wood", ["has_wood", "know_most_wood_position"]);
+    this.expertSystem.addRule("random_move", ["not_enough_info"]);
+    this.expertSystem.addRule("update_info_from_termite", ["perceived_termite"]);
+    this.expertSystem.addRule("update_info_from_heap", ["perceived_heap"]);
 };
 
 Termite.prototype.update = function(dt) {
     this.perceive();
     var conclusions = this.analyze();
-    this.act(conclusions);
-    this.resetCollidedAgent();
+    this.act(conclusions, dt);
+    this.reset();
 };
 
 Termite.prototype.perceive = function() {
     this.expertSystem.resetFactValues();
 
-    this.expertSystem.setFactValid("collided_heap", this.isCollidedWith("wood_heap"));
+    this.expertSystem.setFactValid("has_no_wood", this.hasWood == false);
+    this.expertSystem.setFactValid("has_wood", this.hasWood);
+    this.expertSystem.setFactValid("know_least_wood_position", this.woodInfo.least() != null);
+    this.expertSystem.setFactValid("know_most_wood_position", this.woodInfo.most() != null);
+    this.expertSystem.setFactValid("perceived_termite", this.isPerceivedWith("termite"));
+    this.expertSystem.setFactValid("perceived_heap", this.isPerceivedWith("wood_heap"));
+    this.expertSystem.setFactValid("not_enough_info", this.woodInfo.isEnough() == false);
 };
 
 Termite.prototype.analyze = function() {
     return this.expertSystem.inferForward();
 };
 
-Termite.prototype.act = function(conclusions) {
-    for(var i = 0; i<conclusions.length; i++) {
-        if(conclusions[i] == "") {
-
+Termite.prototype.act = function(conclusions, dt) {
+    if(DEBUG.play) {
+        for (var i = 0; i < conclusions.length; i++) {
+            switch (conclusions[i]) {
+                case "go_to_least_wood":
+                    this.goToHeap(this.woodInfo.least(), dt);
+                    break;
+                case "go_to_most_wood":
+                    this.goToHeap(this.woodInfo.most(), dt);
+                    break;
+                case "update_info_from_termite":
+                    this.updateInfoFromPerceivedTermites();
+                    break;
+                case "update_info_from_heap":
+                    this.updateInfoFromPerceivedHeaps();
+                    break;
+                case "random_move":
+                    this.randomMove(dt);
+                    break;
+            }
+//            console.log(conclusions[i]);
         }
     }
 };
 
 Termite.prototype.isCollidedWith = function(agentType) {
-    return this.collidedAgent == agentType;
+    return this.collidedAgent && this.collidedAgent.typeId == agentType;
 };
 
+Termite.prototype.isPerceivedWith = function(agentType) {
+    for(var i=0; i<this.perceivedAgents.length; i++) {
+        if(this.perceivedAgents[i].typeId == agentType)
+            return true;
+    }
+    return false;
+};
+
+Termite.prototype.getPerceivedAgents = function(agentType) {
+    var perceived = [];
+    for(var i=0; i<this.perceivedAgents.length; i++) {
+        if(this.perceivedAgents[i].typeId == agentType)
+            perceived.push(this.perceivedAgents[i]);
+    }
+    return perceived;
+};
 
 Termite.prototype.processCollision = function(collidedAgent) {
+    this.collidedAgent = collidedAgent;
     if(collidedAgent && collidedAgent.typeId == "wood_heap") {
-        this.collidedAgent = "wood_heap";
         if(!this.hasWood) {
             collidedAgent.takeWood();
+            if(collidedAgent.woodCount <= 0)
+                this.woodInfo.heapDeleted(collidedAgent);
             this.hasWood = true;
         } else {
             collidedAgent.addWood();
@@ -64,10 +113,21 @@ Termite.prototype.processCollision = function(collidedAgent) {
 };
 
 Termite.prototype.processPerception = function(perceivedAgent) {
+    this.perceivedAgents.push(perceivedAgent);
 };
 
-Termite.prototype.resetCollidedAgent = function(){
+Termite.prototype.reset = function(){
     this.collidedAgent = null;
+    this.perceivedAgents = [];
+};
+
+Termite.prototype.move = function(dt) {
+    this.moveBy(this.direction, this.speed*(dt/1000));
+};
+
+Termite.prototype.setTarget = function(x, y) {
+    this.direction = new Vect(x - this.x, y - this.y);
+    this.direction.normalize(1);
 };
 
 Termite.prototype.draw = function(context) {
@@ -80,4 +140,38 @@ Termite.prototype.draw = function(context) {
     context.arc(this.x, this.y, this.boundingRadius, 0, 2*Math.PI);
     context.fill();
     context.stroke();
+};
+
+
+//actions
+Termite.prototype.randomDirection = function() {
+    var x = 2 * Math.random() - 1;
+    var y = 2 * Math.random() - 1;
+    this.direction = new Vect(x, y);
+};
+
+Termite.prototype.randomMove = function (dt) {
+    this.delay -= dt;
+    if(this.delay <= 0) {
+        this.randomDirection();
+        this.delay = 500;
+    }
+    this.move(dt);
+};
+
+Termite.prototype.goToHeap = function(heap, dt) {
+    this.setTarget(heap.x, heap.y);
+    this.move(dt);
+};
+
+Termite.prototype.updateInfoFromPerceivedTermites = function () {
+    var perceivedTermites = this.getPerceivedAgents("termite");
+    for(var i=0; i<perceivedTermites.length; i++)
+        this.woodInfo.update(perceivedTermites[i].woodInfo);
+};
+
+Termite.prototype.updateInfoFromPerceivedHeaps = function() {
+    var perceivedHeaps = this.getPerceivedAgents("wood_heap");
+    for(var i=0; i<perceivedHeaps.length; i++)
+        this.woodInfo.updateHeap(perceivedHeaps[i]);
 };
---
+++ js/wood_info.js
@@ -0,0 +1,90 @@
+WoodInfo = function () {
+    this.heapsInfo = {};
+    this.date = new Date();
+};
+
+WoodInfo.prototype.most = function () {
+    return this.heapWithMostWood;
+};
+
+WoodInfo.prototype.least = function() {
+    return this.heapWithLeastWood;
+};
+
+WoodInfo.prototype.updateHeap = function(heap) {
+    if(heap.woodCount > 0) {
+        if (!this.contains(heap)) {
+            this.addHeapInfo(heap);
+            this.updateLeastAndMostHeap();
+        } else {
+            if (!this.isHeapInfoUpToDate(heap)) {
+                this.updateHeapInfo(heap);
+            }
+        }
+        this.date = new Date();
+    }
+};
+
+WoodInfo.prototype.update = function(woodInfo) {
+    if(woodInfo.date > this.date) {
+        this.heapsInfo = woodInfo.heapsInfo;
+        this.updateLeastAndMostHeap();
+        this.date = woodInfo.date;
+    }
+};
+
+WoodInfo.prototype.isEnough = function() {
+    return this.heapWithMostWood != null && this.heapWithLeastWood != null;
+};
+
+WoodInfo.prototype.contains = function (heap) {
+    return this.getHeapInfo(heap) !== undefined;
+};
+
+WoodInfo.prototype.addHeapInfo = function(heap) {
+    this.heapsInfo[heap.identifier] = {
+        date: new Date(),
+        woodCount: heap.woodCount,
+        x: heap.x,
+        y: heap.y
+    }
+};
+
+WoodInfo.prototype.heapDeleted = function(heap) {
+    delete this.heapsInfo[heap.identifier];
+    this.updateLeastAndMostHeap();
+    this.date = new Date();
+};
+
+WoodInfo.prototype.isHeapInfoUpToDate = function(heap) {
+    var currentHeapInfo = this.getHeapInfo(heap);
+    return currentHeapInfo.woodCount == heap.woodCount;
+};
+
+WoodInfo.prototype.updateHeapInfo = function(heap) {
+    this.addHeapInfo(heap);
+};
+
+WoodInfo.prototype.updateLeastAndMostHeap = function() {
+    //if has at least 2 heaps info
+    if(Object.keys(this.heapsInfo).length >= 2) {
+        var leastWood = null;
+        var mostWood = null;
+        for (var identifier in this.heapsInfo) {
+            var heapInfo = this.heapsInfo[identifier];
+            if(!mostWood || heapInfo.woodCount > mostWood.woodCount)
+                mostWood = heapInfo;
+            if(!leastWood || heapInfo.woodCount < leastWood.woodCount)
+                leastWood = heapInfo;
+        }
+        this.heapWithLeastWood = leastWood;
+        this.heapWithMostWood = mostWood;
+    } else {
+        this.heapWithLeastWood = null;
+        this.heapWithMostWood = null;
+    }
+};
+
+WoodInfo.prototype.getHeapInfo = function(heap) {
+    return this.heapsInfo[heap.identifier];
+};
