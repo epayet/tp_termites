edfa92ce2e9188761a743a9624a96e7c6859c5b0
jmgolfier <jmgolfier@gmail.com>
2014-06-15 06:59:32 +0200

    flagelation : commit de code qui ne compile pas!!! Teste Woodinfos

--- test/WoodInfoSpec.js
+++ test/WoodInfoSpec.js
@@ -6,26 +6,42 @@ describe("A WoodInfo", function () {
     });
 
     describe("initialisation", function() {
-        it("should create a WoodInfo", function () {
+        it("should create a WoodInfoBack", function () {
             expect(woodInfo).toBeDefined();
         });
     });
 
+    describe("update woodinfos", function(){
+        it("should update its info when encountering another woodInfo more recent", function(){
 
-    it("should update its info when encountering another woodInfo more recent");
-    it("should not update its info when encountering another woodInfo less recent");
+        });
+        it("should not update its info when encountering another woodInfo less recent");
+    });
 
-    it("should add heap info when encountering a heap for the first time");
-    it("should update its heap info when encountering a heap encountered before");
-    it("should not add heap info when the heap encountered has no wood");
-
+    describe("update heap", function(){
+        it("should add heap info when encountering a heap for the first time", function(){
+           var heap = {
+               identifier : 1,
+               x : 2,
+               y : 7,
+               woodCount : 10
+           };
+            woodInfo.updateHeap(heap);
+            var heapinfo = woodInfo.heapsInfo[heap.identifier];
+            expect(heapinfo).toBeDefined();
+            expect(heapinfo.x).toBe(heap.x);
+            expect(heapinfo.y).toBe(heap.y);
+            expect(heapinfo.woodCount).toBe(heap.woodCount);
+            expect(heapinfo.date).toBeDefined();
+        });
+        it("should update its heap info when encountering a heap encountered before");
+    });
     it("should have most and least null by default", function () {
         expect(woodInfo.least()).toBeUndefined();
         expect(woodInfo.most()).toBeUndefined();
     });
 
     it("should have correct most and least woodHeaps when defining at least 2 different wood heaps", function() {
-        //woodInfo.updateLeastAndMost
     });
     it("should have most and least undefined when only one heapInfo is defined");
 
@@ -34,12 +50,4 @@ describe("A WoodInfo", function () {
     });
 
     it("should be enough info with at least 2 heaps defined (least and most)");
-
-    it("Should add heap info");
-    //addHeapInfo
-    //updateHeapInfo
-    //heapDeleted
-    //contains(heap)
-    //isHeapInfoUpToDate
-    //getHeapInfo
 });
