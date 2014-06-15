e21b0641d263396cdbdf8b6826e18012980cdc41
epayet <jakspinning@gmail.com>
2014-06-15 09:46:50 +0200

    woodInfo ping pong (jmgolfier & jakspinning)

--- src/info/wood_info.js
+++ src/info/wood_info.js
@@ -4,27 +4,99 @@ WoodInfo = function () {
 };
 
 WoodInfo.prototype.most = function () {
-    //return le plus gros
+    return this.heapWithMostWood;
 };
 
 WoodInfo.prototype.least = function() {
-    // return le plus petit
+    return this.heapWithLeastWood;
 };
 
 WoodInfo.prototype.updateHeap = function(heap) {
     // quand rencontre tas de bois update de la list if premier rencontre on add sinon update woodinfo et date
-    this.heapsInfo[{identifier : }] = heap.identifier
+    if(!this.heapsInfo[heap.identifier]){
+        this.heapsInfo[heap.identifier] = {
+            x: heap.x,
+            y: heap.y,
+            woodCount: heap.woodCount,
+            date : new Date()
+        };
+    }
+    else{
+        var heapInfo = this.heapsInfo[heap.identifier];
+        heapInfo.x = heap.x;
+        heapInfo.y = heap.y;
+        heapInfo.woodCount = heap.woodCount;
+        heapInfo.date = new Date();
+    }
+    this.recomputeMostAndLeast();
 };
 
 WoodInfo.prototype.update = function(woodInfo) {
-    // rencontre une autre termites on prend les infos les plus a jours dans l'autre termites
-
+    for(var identifier in woodInfo.heapsInfo) {
+        var heapInfo2 = woodInfo.heapsInfo[identifier];
+        if(!this.heapsInfo[identifier]) {
+            this.heapsInfo[identifier] = {
+                x: heapInfo2.x,
+                y: heapInfo2.y,
+                woodCount: heapInfo2.woodCount,
+                dead: heapInfo2.dead,
+                date : heapInfo2.date
+            };
+            this.recomputeMostAndLeast();
+        }
+        else{
+            var heapInfo = this.heapsInfo[identifier];
+            if(heapInfo2.date > heapInfo.date) {
+                heapInfo.x = heapInfo2.x;
+                heapInfo.y = heapInfo2.y;
+                heapInfo.woodCount = heapInfo2.woodCount;
+                heapInfo.date = heapInfo2.date;
+                heapInfo.dead = heapInfo2.dead;
+                this.recomputeMostAndLeast();
+            }
+        }
+    }
 };
 
 WoodInfo.prototype.isEnough = function() {
-    // if connaitre most en least
+    return this.most()!= null && this.least() != null;
 };
 
 WoodInfo.prototype.heapDeleted = function(heap) {
-    // quand un tas de bois est vider update des infos
+    var heapInfo = this.heapsInfo[heap.identifier];
+    heapInfo.dead = true;
+    heapInfo.date = new Date();
+    this.recomputeMostAndLeast();
+};
+
+WoodInfo.prototype.recomputeMostAndLeast = function () {
+    if(this.getNbAliveHeaps() >= 2) {
+        var leastWood = null;
+        var mostWood = null;
+        for (var identifier in this.heapsInfo) {
+            var heapInfo = this.heapsInfo[identifier];
+            if(!heapInfo.dead){
+                if(!mostWood || heapInfo.woodCount > mostWood.woodCount)
+                    mostWood = heapInfo;
+                if(!leastWood || heapInfo.woodCount < leastWood.woodCount)
+                    leastWood = heapInfo;
+            }
+        }
+        this.heapWithLeastWood = leastWood;
+        this.heapWithMostWood = mostWood;
+    } else {
+        this.heapWithLeastWood = null;
+        this.heapWithMostWood = null;
+    }
+};
+
+WoodInfo.prototype.getNbAliveHeaps = function() {
+    var nbAlive = 0;
+    for(var identifier in this.heapsInfo){
+        if(!this.heapsInfo[identifier].dead){
+            nbAlive++;
+        }
+    }
+    return nbAlive;
+
 };
--- test/WoodInfoSpec.js
+++ test/WoodInfoSpec.js
@@ -1,53 +1,166 @@
 describe("A WoodInfo", function () {
-    var woodInfo;
+    var woodInfo, woodInfo2;
 
     beforeEach(function () {
         woodInfo = new WoodInfo();
+        woodInfo2 = new WoodInfo();
     });
 
-    describe("initialisation", function() {
-        it("should create a WoodInfoBack", function () {
+    describe("initialization", function() {
+        it("should create a WoodInfo", function () {
             expect(woodInfo).toBeDefined();
         });
     });
 
-    describe("update woodinfos", function(){
+    describe("update woodInfo", function(){
         it("should update its info when encountering another woodInfo more recent", function(){
-
+            var heap = createHeap(2 ,7 ,10);
+            woodInfo2.updateHeap(heap);
+            woodInfo.update(woodInfo2);
+            var heapInfo = woodInfo.heapsInfo[heap.identifier];
+            expect(heapInfo).toBeDefined();
         });
-        it("should not update its info when encountering another woodInfo less recent");
+
+        it("should update its info when encountering another woodInfo more recent (existing)", function () {
+            var heap = createHeap(2 ,7 ,10);
+            woodInfo.updateHeap(heap);
+            var heapInfo = woodInfo.heapsInfo[heap.identifier];
+            heapInfo.date = new Date(2010, 10, 10);
+
+            heap.woodCount--;
+            woodInfo2.updateHeap(heap);
+
+            woodInfo.update(woodInfo2);
+            expect(heapInfo.woodCount).toBe(heap.woodCount);
+        });
+
+        it("should not update its info when encountering another woodInfo less recent", function(){
+            var heap = createHeap(2, 7, 10);
+            woodInfo2.updateHeap(heap);
+            var heapInfo2 = woodInfo2.heapsInfo[heap.identifier];
+            heapInfo2.date = new Date(2010, 10, 10);
+
+            heap.woodCount--;
+            woodInfo.updateHeap(heap);
+
+            var heapInfo = woodInfo.heapsInfo[heap.identifier];
+            woodInfo.update(woodInfo2);
+            expect(heapInfo.woodCount).toBe(heap.woodCount);
+        });
+
     });
 
     describe("update heap", function(){
         it("should add heap info when encountering a heap for the first time", function(){
-           var heap = {
-               identifier : 1,
-               x : 2,
-               y : 7,
-               woodCount : 10
-           };
+            var heap = createHeap(2 ,7 ,10);
             woodInfo.updateHeap(heap);
-            var heapinfo = woodInfo.heapsInfo[heap.identifier];
-            expect(heapinfo).toBeDefined();
-            expect(heapinfo.x).toBe(heap.x);
-            expect(heapinfo.y).toBe(heap.y);
-            expect(heapinfo.woodCount).toBe(heap.woodCount);
-            expect(heapinfo.date).toBeDefined();
+            var heapInfo = woodInfo.heapsInfo[heap.identifier];
+            expect(heapInfo).toBeDefined();
+            expect(heapInfo.x).toBe(heap.x);
+            expect(heapInfo.y).toBe(heap.y);
+            expect(heapInfo.woodCount).toBe(heap.woodCount);
+            expect(heapInfo.date).toBeDefined();
         });
-        it("should update its heap info when encountering a heap encountered before");
-    });
-    it("should have most and least null by default", function () {
-        expect(woodInfo.least()).toBeUndefined();
-        expect(woodInfo.most()).toBeUndefined();
+
+        it("should update its heap info when encountering a heap encountered before", function() {
+            var heap = createHeap(2 ,7 ,10);
+            woodInfo.updateHeap(heap);
+            var heapInfo = woodInfo.heapsInfo[heap.identifier];
+            expect(heapInfo.woodCount).toBe(heap.woodCount);
+
+            heap.woodCount--;
+            woodInfo.updateHeap(heap);
+            expect(heapInfo.woodCount).toBe(heap.woodCount);
+        });
     });
 
-    it("should have correct most and least woodHeaps when defining at least 2 different wood heaps", function() {
-    });
-    it("should have most and least undefined when only one heapInfo is defined");
+    describe("most and least", function () {
+        it("should have most and least null by default", function () {
+            expect(woodInfo.isEnough()).toBe(false);
+            expect(woodInfo.least()).toBeUndefined();
+            expect(woodInfo.most()).toBeUndefined();
+        });
 
-    it("should not be enough info by default", function() {
-        expect(woodInfo.isEnough()).toBe(false);
+        it("should have correct most and least woodHeaps when defining at least 2 different wood heaps", function() {
+            var heapLeast = createHeap(20, 20, 10);
+            var heapMost = createHeap(50, 50, 20);
+            var heapMost1 = createHeap(50, 50, 17);
+            var heapMost2 = createHeap(50, 50, 12);
+            woodInfo.updateHeap(heapLeast);
+            woodInfo.updateHeap(heapMost);
+            woodInfo.updateHeap(heapMost1);
+            woodInfo.updateHeap(heapMost2);
+
+            expect(woodInfo.isEnough()).toBe(true);
+            expect(woodInfo.least().woodCount).toBe(10);
+            expect(woodInfo.most().woodCount).toBe(20);
+        });
+
+        it("should have most and least undefined when only one heapInfo is defined", function(){
+            var heapTheOnlyOne = createHeap(20, 20, 20);
+            woodInfo.updateHeap(heapTheOnlyOne);
+
+            expect(woodInfo.isEnough()).toBe(false);
+            expect(woodInfo.least()).toBe(null);
+            expect(woodInfo.most()).toBe(null);
+        });
     });
 
-    it("should be enough info with at least 2 heaps defined (least and most)");
+    describe("heap deleted", function () {
+        it("should be dead in the info", function(){
+            var deadHeap = createHeap(20, 20, 1);
+            woodInfo.updateHeap(deadHeap);
+            deadHeap.kill();
+            woodInfo.heapDeleted(deadHeap);
+            var heapInfo = woodInfo.heapsInfo[deadHeap.identifier];
+            expect(heapInfo.dead).toBe(true);
+        });
+
+        it("should update dead info with another termite", function () {
+            var deadHeap = createHeap(20, 20, 1);
+            woodInfo.updateHeap(deadHeap);
+            deadHeap.kill();
+            woodInfo.heapDeleted(deadHeap);
+            woodInfo2.update(woodInfo);
+            var heapInfo2 = woodInfo2.heapsInfo[deadHeap.identifier];
+            expect(heapInfo2.dead).toBe(true);
+        });
+
+        it("should consider dead heaps in recomputing", function() {
+            var heapLeast = createHeap(10, 10, 2);
+            var heapMost = createHeap(10, 10, 10);
+            woodInfo.updateHeap(heapLeast);
+            woodInfo.updateHeap(heapMost);
+            var deadHeap = createHeap(20, 20, 1);
+            woodInfo.updateHeap(deadHeap);
+            deadHeap.kill();
+            woodInfo.heapDeleted(deadHeap);
+            expect(woodInfo.most().woodCount).toBe(10);
+            expect(woodInfo.least().woodCount).toBe(2);
+        });
+
+        it("should have most and least undefined when one dead", function () {
+            var heapMost = createHeap(10, 10, 2);
+            woodInfo.updateHeap(heapMost);
+            var deadHeap = createHeap(20, 20, 1);
+            woodInfo.updateHeap(deadHeap);
+            deadHeap.kill();
+            woodInfo.heapDeleted(deadHeap);
+            expect(woodInfo.most()).toBe(null);
+            expect(woodInfo.least()).toBe(null);
+        });
+    });
+
 });
+function createHeap(x, y, woodCount){
+    return {
+        identifier : Math.random()*1000,
+        x : x,
+        y : y,
+        woodCount : woodCount,
+        kill: function() {
+            this.woodCount--;
+            this.dead = true;
+        }
+    };
+}
